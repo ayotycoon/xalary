fe1578ceb9a4c87e5c8ab904b6ec70a4590acc66
Ayokunle Sunmola <ayokunle_sunmola@intuit.com>
2023-01-23 14:09:45 -0600

    first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: `/xalary`,
   siteMetadata: {
     title: `traka`,
     siteUrl: `https://www.yourdomain.tld`
